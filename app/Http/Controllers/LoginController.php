908c02506d9b0a609690608818c5d30c52c0148a
Sandeep <sandeeptwr04@gmail.com>
2021-07-08 00:36:23 +0530

    formating

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,34 +9,33 @@ use Auth;
 class LoginController extends Controller
 {
     //
-	public function store(Request $request)
-	{
-	
-	   $validator = Validator::make($request->all(), [
-        'uname' => 'required|email',
-        'psw' => 'required',
-    ]);
-	if ($validator->fails()) {
-		  return redirect('admin')
-                        ->withErrors($validator)
-                        ->withInput();
-		}
-		//dd(auth()->attempt(array('email' =>$request['uname'], 'password' => $request['psw'])));
-	if(auth()->attempt(array('email' =>$request['uname'], 'password' => $request['psw'])))
-    {
-	  
-        if (auth()->user()) {
-            return redirect('dashboard');
-        }else{
-            return redirect('admin');
+    public function store(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+                    'uname' => 'required|email',
+                    'psw' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return redirect('admin')
+                            ->withErrors($validator)
+                            ->withInput();
         }
-    }else{
-        return redirect('admin')->with('error','Email-Address And Password Are Wrong.');
-            
+        //dd(auth()->attempt(array('email' =>$request['uname'], 'password' => $request['psw'])));
+        if (auth()->attempt(array('email' => $request['uname'], 'password' => $request['psw']))) {
+
+            if (auth()->user()) {
+                return redirect('dashboard');
+            } else {
+                return redirect('admin');
+            }
+        } else {
+            return redirect('admin')->with('error', 'Email-Address And Password Are Wrong.');
+        }
     }
-	}
-	public function logout() {
-       Auth::logout();
-      return redirect('/admin');
-   }
+
+    public function logout() {
+        Auth::logout();
+        return redirect('/admin');
+    }
+
 }
